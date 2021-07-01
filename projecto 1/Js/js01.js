da1d1fcc3f18f298a757aa34113f088e3dcaf933
james <airesandate@gmail.com>
2021-07-01 18:47:10 +0200

    001

---
+++ projecto 1/Js/js01.js
@@ -0,0 +1,15 @@
+alert('ola');
+
+function check_info() {
+	var Eemal = document.getElementById('Eemal').value;
+	var pass = document.getElementById('pass').value;
+
+	if (E_emal== || pass==) {
+        alert("Por favar preencha os campos");
+        return false;
+	}
+	else{
+	
+		return true; 
+	}
+} 
