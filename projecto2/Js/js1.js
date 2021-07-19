87df68a6a78c3b55762dfb97af59c8d4a54308bb
james <airesandate@gmail.com>
2021-07-19 17:04:24 +0200

    p2
    projecto2

---
+++ projecto2/Js/js1.js
@@ -0,0 +1,17 @@
+//alert("esta a foncionar ");
+var index=0;
+show();
+function show(){
+	var i;
+	var slides=document.getElementsByClassName("slide");
+	for(i=0;i<slides.length;i++){
+		slides[i].style.display="none";
+	}
+	index=index+1;
+	if(index>slides.length){
+		index=1;
+
+	}
+	slides[index-1].style.display="block";
+	setTimeout(show,1500);
+}
